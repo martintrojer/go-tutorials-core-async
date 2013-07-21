5a2d9509be83874b730fef79ccfcfc5e9cc3fdb9
Martin Trojer <martin.trojer@gmail.com>
2013-07-21 14:02:07 +0100

    one atom less

--- src/go_tutorials_core_async/tut200.clj
+++ src/go_tutorials_core_async/tut200.clj
@@ -7,17 +7,16 @@
   (go
    (loop []
      (let [ball (<! table)]
-       (swap! ball inc)
-       (println name @ball)
+       (println name ball)
        (<! (timeout (rand-int 100)))
-       (>! table ball)
+       (>! table (inc ball))
        (recur)))))
 
 (let [table (chan)]
   (player "ping" table)
   (player "pong" table)
 
-  (>!! table (atom 0))      ;; go-lang will detect a deadlock if this commented out
+  (>!! table 0)      ;; go-lang will detect a deadlock if this commented out
   (Thread/sleep 1000)
   (<!! table)  ;; take the ball off the table
   (println "game over"))
