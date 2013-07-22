8adc2412c85c44492b43e58a3421e2a89c4584a8
Martin Trojer <martin.trojer@gmail.com>
2013-07-22 12:26:33 +0100

    Sameer's RSS example

--- src/go_tutorials_core_async/http.clj
+++ src/go_tutorials_core_async/http.clj
@@ -1,10 +1,29 @@
 (ns go-tutorials-core-async.http
   (:use [clojure.core.async])
   (:require [clj-http.client]
-            [org.httpkit.client]))
+            [org.httpkit.client]
+            [clojure.xml :as xml]
+            [clojure.zip :as zip]))
 
 (defn blocking-get [url]
   (clj-http.client/get url))
 
 (defn async-get [url result]
   (org.httpkit.client/get url #(go (>! result %))))
+
+(defn get-blog-entries [f url]
+  (letfn [(zip-str [s]
+            (zip/xml-zip (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
+          (get-data [c]
+            (let [get-content (fn [tag] (->> c (filter #(= tag (:tag %))) first :content first))]
+              {:id (get-content :id)
+               :title (get-content :title)
+               :body (get-content :content)}))]
+    (->> url
+         f
+         :body
+         zip-str
+         zip/children
+         (filter #(= :entry (:tag %)))
+         (map :content)
+         (map get-data))))
--- src/go_tutorials_core_async/tut201.clj
+++ src/go_tutorials_core_async/tut201.clj
@@ -1,16 +1,50 @@
-;; http://talks.golang.org/2013/advconc.slide#14
+;; http://talks.golang.org/2013/advconc.slide#14 (and later)
 
 (ns go-tutorials-core-async.tut201
   (:use [clojure.core.async]
+        [clojure.set]
         [go-tutorials-core-async.http]))
 
-(defn subscription [url interval]
-  (let [updates (chan)
-        quit (chan)]
+(def get-entries (partial get-blog-entries blocking-get))
+
+(defn subscription [url interval max-pending]
+  (let [update-ch (chan)
+        quit-ch (chan)]
+    ;; thread because we using a blocking get call
     (thread
-     (loop [seen #{}]
-       (let [[_ ch] (alts!! [quit (timeout interval)])]
-         (when-not (= ch quit)
-           (>!! updates (blocking-get url))
-           (recur seen)))))
-    [updates quit]))
+     (loop [[fst & rst :as updates] (get-entries url)
+            seen-ids (->> updates (map :id) (into #{}))]
+       (let [query [quit-ch (timeout interval)]
+             query (if-not (nil? fst) (conj query [update-ch fst]) ;; potentially append the put query
+                           query)
+             [_ ch] (alts!! query)]
+         (cond
+          (= ch quit-ch) (close! update-ch)
+          (= ch update-ch) (recur rst seen-ids)
+          :else (let [entries (get-entries url)
+                      new-entries (remove #(seen-ids (:id %)) entries)]
+                  (println "got" (count entries) ": new" (count new-entries) ":" url)
+                  (recur (take max-pending (concat updates new-entries))
+                         (union seen-ids (->> new-entries (map :id) (into #{}))))))))
+     (println url "subscriber quitting"))
+    [update-ch quit-ch]))
+
+(defn fan-in [& ins]
+  (let [ch (chan)]
+    (doseq [i ins]
+      (go (loop [v (<! i)]
+            (when v (>! ch v))
+            (recur (<! i)))))
+    ch))
+
+(let [feeds ["http://feeds.feedburner.com/GDBcode?format=xml"
+             "http://blog.golang.org/feeds/posts/default"
+             "http://feeds.feedburner.com/GoogleOpenSourceBlog?format=xml"]
+      chs (mapv #(subscription % 1000 500) feeds)
+      all-ch (apply fan-in (map first chs))]
+  (dotimes [_ 5]
+    (println (<!! all-ch))
+    (Thread/sleep 1000))
+  (close! all-ch)
+  (doseq [qc (map second chs)]
+    (>!! qc true)))
