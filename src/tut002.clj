576c89f7a717bd6b770026348025ed207ecda59a
Martin Trojer <martin.trojer@gmail.com>
2014-03-09 09:06:33 +0000

    cleanup

--- src/tut002.clj
+++ src/tut002.clj
@@ -1,15 +1,15 @@
 ;; http://tour.golang.org/#63
 
 (ns tut2
-  (:require [clojure.core.async :refer [go chan <! <!!]]))
+  (:require [clojure.core.async :refer [chan put! <!!]]))
 
 (defn sum [vals ch]
-  (go (>! ch (clojure.core/reduce + vals))))
+  (put! ch (clojure.core/reduce + vals)))
 
 (let [vals [7 2 8 -9 4 0]
       ch (chan)]
-  (go (sum (take 3 vals) ch))
-  (go (sum (drop 3 vals) ch))
+  (sum (take 3 vals) ch)
+  (sum (drop 3 vals) ch)
 
   (let [x (<!! ch)
         y (<!! ch)]
